b3508ddc97973b0cdb07d4f7dc7ff2daa6e31f95
Silvy Dharma Febryana <SF40203X@cimbniaga.co.id>
2020-06-22 13:53:36 +0700

    verifikasi email

--- tokolapak/tokolapak/src/main/java/com/cimb/tokolapak/entity/User.java
+++ tokolapak/tokolapak/src/main/java/com/cimb/tokolapak/entity/User.java
@@ -16,7 +16,12 @@ public class User {
     private String username;
   
     private String password;
-    
+	
+	private String profilePicture;
+
+	private String email;
+
+	private boolean isVerified;
 
 	public int getId() {
 		return id;
@@ -36,6 +41,30 @@ public class User {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getProfilePicture() {
+		return profilePicture;
+	}
+
+	public void setProfilePicture(String profilePicture) {
+		this.profilePicture = profilePicture;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isVerified() {
+		return isVerified;
+	}
+
+	public void setVerified(boolean isVerified) {
+		this.isVerified = isVerified;
 	}
 	
 	
